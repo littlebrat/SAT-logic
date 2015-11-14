34d32a63b1086b5f5a7ec1738249161840eddf5f
Nuno Mendes <mrgugas1993@gmail.com>
2015-11-14 16:55:53 +0000

    Initial structure started

---
+++ src/structure/clause.py
@@ -0,0 +1,5 @@
+
+
+class Clause:
+
+    def __init__(self, dict):
---
+++ src/structure/kb.py
@@ -0,0 +1,29 @@
+from src.structure.clause import Clause
+
+class KnowledgeBase:
+
+    def __init__(self):
+        self.variables = []
+        self.clauses = []
+
+    @staticmethod
+    def from_file(self,path):
+        kb = KnowledgeBase()
+        # this method loads a file into a object of this class.
+        with open(path) as file:
+            for line in file:
+                # for each line in the file verify if the structure of the line is correct.
+                words = line.split()
+                if words[0] is 'c':
+                    # comments line should be ignored
+                    continue
+                if words[0] is 'p' and words[1] is 'cnf' and len(words) == 4:
+                    # read format
+                    self.variables = range(1,int(words[2])+1)
+                    clauses = int(words[3])
+                else:
+                    aux = words[:-1]
+                    cl = Clause(aux)
+                    self.clauses.append(cl)
+        return kb
+
