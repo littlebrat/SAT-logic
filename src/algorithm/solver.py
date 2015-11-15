fbc556377dc29c01af8812506d6b6bd92684bfdd
RSimas <ricardo.d.simas@ist.utl.pt>
2015-11-15 00:41:23 +0000

    GSAT Done

--- src/algorithm/solver.py
+++ src/algorithm/solver.py
@@ -1,6 +1,7 @@
 from src.structure.sentence import Sentence
 from src.structure.clause import Clause
 from random import getrandbits
+import operator
 
 class Algorithms:
 	"""
@@ -13,18 +14,28 @@ class Algorithms:
 	@staticmethod
 	def gsat(sentence, max_restarts, max_climbs):
 		for i in max_restarts:
-			#for each literal assign a random boolean
-			for k in variables:
-				A[k] = not getrandbits(1)
-			#check if the current set of literals satisfies the sentence
+			e=0 #e is a variable that is used to run through A to change each symbol's value
+			#for each symbol assign a random boolean
+			for k in variables: #variables is the number of different symbols in the sentence
+				A[k] = not getrandbits(1) #A is the set of literals
+			aux = A
 			for j in max_climbs:
-				sentence = conjunction(A) #function that does the conjunction of clauses and returns true or false for the sentence
-				if sentence is True:
+				s = conjunction(A) #function that does the conjunction of clauses and returns true or false for the sentence (s)
+				#check if the current set of literals satisfies the sentence
+				if s is True:
 					return A
 					break
+				else:
+					for e is variables:
+						A[e] = not A[e] #change the value of a symbol
+						evaluationscore[e] = evaluation(A) #evaluation function to determine the operator that yields the best evaluation
+						A = aux #revert the previous symbol change
+					max_index, max_value = max(enumerate(evaluationscore), key=operator.itemgetter(1)) #find the best evaluation score and it's index
+					aux[max_index] = not aux[max_index]
+					A = aux #change the value of the symbol, which provides the best evaluation score, permanently
+					e=0
 		return Exception('Failure on GSAT')
 					
-
-
-		@staticmethod
-		def gsat
+	#@staticmethod
+	#def walksat(clauses,p,max_flips):
+		
