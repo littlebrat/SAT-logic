14397fc7c232320b936be29cf39cf0d4cc354130
RSimas <ricardo.d.simas@ist.utl.pt>
2015-11-15 01:37:33 +0000

    GSAT done (and now its more efficient)

--- src/algorithm/solver.py
+++ src/algorithm/solver.py
@@ -1,7 +1,6 @@
 from src.structure.sentence import Sentence
 from src.structure.clause import Clause
 from random import getrandbits
-import operator
 
 class Algorithms:
 	"""
@@ -14,26 +13,30 @@ class Algorithms:
 	@staticmethod
 	def gsat(sentence, max_restarts, max_climbs):
 		for i in max_restarts:
-			e=0 #e is a variable that is used to run through A to change each symbol's value
 			#for each symbol assign a random boolean
 			for k in variables: #variables is the number of different symbols in the sentence
 				A[k] = not getrandbits(1) #A is the set of literals
 			aux = A
 			for j in max_climbs:
+				literal=0 #literal is a variable that is used to run through A to change each symbol's value
 				s = conjunction(A) #function that does the conjunction of clauses and returns true or false for the sentence (s)
 				#check if the current set of literals satisfies the sentence
 				if s is True:
 					return A
 					break
 				else:
-					for e is variables:
-						A[e] = not A[e] #change the value of a symbol
-						evaluationscore[e] = evaluation(A) #evaluation function to determine the operator that yields the best evaluation
+					max_score = 0 #max_score, which contains the best evaluation score, is initialized
+					index = -1
+					for literal in variables:
+						A[literal] = not A[literal] #change the value of a symbol
+						evaluationscore[literal] = evaluation(A) #evaluation function to determine the operator that yields the best evaluation
+						if evaluationscore[literal]>max_score #check the current best evaluation score and it's index
+							max_score = evaluationscore[literal]
+							index = literal
 						A = aux #revert the previous symbol change
-					max_index, max_value = max(enumerate(evaluationscore), key=operator.itemgetter(1)) #find the best evaluation score and it's index
-					aux[max_index] = not aux[max_index]
-					A = aux #change the value of the symbol, which provides the best evaluation score, permanently
-					e=0
+					if index != -1 #if an evaluation score was found (at least one clause is true) then a symbol is permanently changed
+						aux[index] = not aux[index]
+						A = aux #change the value of the symbol, which provides the best evaluation score, permanently
 		return Exception('Failure on GSAT')
 					
 	#@staticmethod
