2b868a1d75d60169cb2791c51ff85b5fac14a5a8
littlebrat <gugas93@hotmail.com>
2015-11-17 14:02:13 +0000

    Added real methods

--- src/algorithm/solver.py
+++ src/algorithm/solver.py
@@ -1,6 +1,9 @@
 from src.structure.sentence import Sentence
 from src.structure.solution import Solution
-import random
+from src.structure.clause import Clause
+from random import choice
+from random import random
+
 
 class Algorithms:
     """
@@ -29,42 +32,47 @@ class Algorithms:
                             max_score = score
                             best = literal
                         sl.toggle(literal)
-                    if best != None:
+                    if best is not None:
                         sl.toggle(best)
         return 'No solution was found.'
 
     def walksat(sentence, p, max_flips):
-        sl = Solution(sentence.variables) #sl is the set of symbols in the sentence
-        sl.restart() #sl is now the model which means that there was a random assignment of true/false to the symbols in clauses
+        # Instatiate the model for the problem
+        sl = Solution(sentence.variables)
+        # For each symbol assign a random boolean.
+        sl.restart()
         for i in range(max_flips):
             # Checks if sentence is satisfied by the current values of literals in solution object.
             if sentence.is_satisfied(sl):
                 return sl
             else:
+                # Get the list of false clauses from the model and sentence.
+                falses_clauses = sentence.false_clauses(sl)
                 # Choose a random false clause from the sentence.
-                clause = Randomfalseclause()  # it still does not exist. i want this in symbols not in booleans.
+                false_cl = choice(falses_clauses)
                 # with probability p flip the value of a randomly selected symbol from clause.
-                if p>random.random():
-                    index = random.choice(clause)
-                    sl.toggle[abs(index)-1]
-                # flip whichever symbol in clause that maximizes the number of satisfied clauses.
+                if p > random():
+                    random_literal = choice(false_cl.get_literals())
+                    sl.toggle(random_literal)
                 else:
+                    # flip whichever symbol in clause that maximizes the number of satisfied clauses.
                     # Variable to track the best score for satisfied clauses.
                     max_score = sentence.get_validated_clauses(sl)
                     # Variable to store the literal attribution of the best score.
                     best = None
-                    for literal in range(len(clause)):
-                        sl.toggle(abs(clause[literal])-1)
+                    for literal in false_cl.get_literals():
+                        sl.toggle(literal)
                         score = sentence.get_validated_clauses(sl)
                         if score > max_score:
                             max_score = score
-                            best = abs(clause[literal])-1
-                        sl.toggle(abs(clause[literal])-1)
+                            best = literal
+                        sl.toggle(literal)
                     if best != None:
                         sl.toggle(best)
         return 'No solution was found.'
 
-sent = Sentence.from_file('../test_files/charmander.txt')
-r = Algorithms.gsat(sent, 50, 20)
+sent = Sentence.from_file('../test_files/pokemon/charmander.txt')
+# r = Algorithms.gsat(sent, 50, 20)
+r = Algorithms.walksat(sent, 0.5, 20)
 print(r)
 
